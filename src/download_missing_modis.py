063bea057ed520f2d0f6f3b14e9b795b37748132
Unnsteinsson <r01tu23@abdn.ac.uk>
2024-11-12 14:11:44 -0900

    download missing data

--- src/download_missing_modis.py
+++ src/download_missing_modis.py
@@ -27,14 +27,12 @@ products = {
     "MYD35_L2": f"MODIS_Cloud_mask_{args.name}_Aqua",
 }
 
-# Provide the user's credentials.
-earthaccess.login()
 
-
-def download_product(product, dir_download, bounding_box=args.bbox, temporal=args.temp, count=-1):
+def find_missing_products(product, dir_download, bounding_box=args.bbox, temporal=args.temp, count=-1):
     if not os.path.exists(dir_download):
         os.mkdir(dir_download)
 
+    # Find all MODIS products within the timeframe.
     results = earthaccess.search_data(
         short_name=product,
         bounding_box=bounding_box,
@@ -42,20 +40,55 @@ def download_product(product, dir_download, bounding_box=args.bbox, temporal=arg
         count=count
     )
 
-    earthaccess.download(
-        granules=results,
-        local_path=dir_download
-    )
+    # List of all currently downloaded products.
+    file_list = os.listdir(dir_download)
+
+    i = 0
+    while i < len(results):
+        if os.path.basename(results[i].data_links()[0]) in file_list:
+            results.pop(i)  # Remove products from list that are already downloaded.
+        else:
+            print(" - ", os.path.basename(results[i].data_links()[0]))
+            i += 1
+
+    return results
 
 
 def main():
+    results = {key: [] for key in products.keys()}
+
+    s = 0
     for product in products:
         print(product)
 
-        download_product(
+        results[product] = find_missing_products(
             product=product,
             dir_download=os.path.join(args.dir, products[product])
         )
+
+        if not results[product]:
+            results.pop(product)
+        else:
+            s += len(results[product])
+
+    print()
+    if s == 0:
+        print("No missing scenes found.")
+        return
+
+    # Download the products with user's approval.
+    if input(f"Submit {s} downloads (yes/no):").lower() == "yes":
+
+        # Provide the user's credentials.
+        earthaccess.login()
+
+        for product in results:
+            earthaccess.download(
+                granules=results[product],
+                local_path=os.path.join(args.dir, products[product])
+            )
+    else:
+        print("Download halted by user.")
 
 
 if __name__ == "__main__":
