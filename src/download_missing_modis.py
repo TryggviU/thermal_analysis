c5e5f6455ee79fa427eb4be6dc148ad4261d73f7
Unnsteinsson <r01tu23@abdn.ac.uk>
2024-11-12 15:21:22 -0900

    allow txt input

--- src/download_missing_modis.py
+++ src/download_missing_modis.py
@@ -5,18 +5,29 @@ import earthaccess
 # Initiate the argument parser.
 parser = argparse.ArgumentParser()
 # Add arguments.
-parser.add_argument("-n", "--name", action="store", type=str, required=True,
+parser.add_argument("-f", "--file", action="store", type=str, required=False,
+                    help=".txt file with input parameters.")
+parser.add_argument("-n", "--name", action="store", type=str, required=False,
                     help="The volcano name.")
-parser.add_argument("-b", "--bbox", action="store", nargs=4, type=float, required=True,
+parser.add_argument("-b", "--bbox", action="store", nargs=4, type=float, required=False,
                     help="The bounding box coordinates low_left_lon, low_left_lat, up_right_lon, up_right_lat.")
-parser.add_argument("-t", "--temp", action="store", nargs=2, type=str, required=True,
+parser.add_argument("-t", "--temp", action="store", nargs=2, type=str, required=False,
                     help="The start and end times as YYYY-MM-DD.")
-parser.add_argument("-d", "--dir", action="store", type=str, required=True,
+parser.add_argument("-d", "--ddir", action="store", type=str, required=False,
                     help="The download destination directory.")
 # Read arguments from the command line
 args = parser.parse_args()
-args.bbox = tuple(args.bbox)
-args.temp = tuple(args.temp)
+
+if args.file:
+    f = open(args.file, "r")
+    args.name = f.readline().rstrip("\n")
+    args.bbox = tuple(map(float, f.readline().rstrip("\n").split()))
+    args.temp = tuple(f.readline().rstrip("\n").split())
+    args.ddir = f.readline().rstrip("\n")
+    f.close()
+else:
+    args.bbox = tuple(args.bbox)
+    args.temp = tuple(args.temp)
 
 products = {
     "MOD021KM": f"MODIS_Level_1B_{args.name}_Terra",
@@ -63,7 +74,7 @@ def main():
 
         results[product] = find_missing_products(
             product=product,
-            dir_download=os.path.join(args.dir, products[product])
+            dir_download=os.path.join(args.ddir, products[product])
         )
 
         if not results[product]:
@@ -85,7 +96,7 @@ def main():
         for product in results:
             earthaccess.download(
                 granules=results[product],
-                local_path=os.path.join(args.dir, products[product])
+                local_path=os.path.join(args.ddir, products[product])
             )
     else:
         print("Download halted by user.")
